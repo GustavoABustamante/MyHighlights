1c1da167d037bb95990e1ea089be4053d27172b7
GUSTAVO ADOLFO BUSTAMANTE BUSTAMANTE <gustavobustamante1996@gmail.com>
2022-03-22 21:19:28 -0300

    Agregando policy agreements checkbox en formulario de registro

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  safelist: ["bg-blue-400", "bg-green-400", "bg-red-400"],
+  theme: {
+    extend: {
+      minHeight: {
+        182: "48rem",
+      },
+    },
+  },
+  plugins: [],
+};
